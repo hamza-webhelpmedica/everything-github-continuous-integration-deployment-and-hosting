5430e1a67a7c2ed09d9060d09219afa7b86e3022
hamza-webhelpmedica <hamza.bedoui@webhelpmedica.com>
2020-02-26 12:06:46 +0100

    fix lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,11 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {  
-  book: any = {};
+export class AppComponent {
+  book: any = { };
+
   constructor(http: HttpClient) {
     http.get('https://api.angular.schule/book/9783864906466')
       .subscribe(b => this.book = b);
   }
-
 }

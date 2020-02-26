559e3ceb48e0607dfd238cc82d180a25bf7f6187
Hamza BEDOUI <hbedoui@directmedica.com>
2020-02-26 10:15:42 +0100

    init project demo angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'everything-github-demo';
+  book: any = {};
+
+  constructor(http: HttpClient){    
+    http.get('https://api.angular.schule/book/9783864906466')
+        .subscribe(b => this.book = b);
+  }
+
 }
